5b641a2eb9d8483c3aec0c50ab60ee60b0a5ab30
Benny Barcellos <bbarcell@gmail.com>
2018-08-28 12:33:38 -0700

    Minor changes and added exceptions.

---
+++ SwitchManager/nx/cdn/CnmtMissingException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SwitchManager.nx.cdn
+{
+    [Serializable]
+    internal class CnmtMissingException : Exception
+    {
+        public CnmtMissingException()
+        {
+        }
+
+        public CnmtMissingException(string message) : base(message)
+        {
+        }
+
+        public CnmtMissingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CnmtMissingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ SwitchManager/nx/cdn/DownloadFailedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SwitchManager.nx.cdn
+{
+    [Serializable]
+    internal class DownloadFailedException : Exception
+    {
+        public DownloadFailedException()
+        {
+        }
+
+        public DownloadFailedException(string message) : base(message)
+        {
+        }
+
+        public DownloadFailedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DownloadFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
